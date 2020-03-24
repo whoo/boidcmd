f2d006938f3b00ebc9b340dcde9f7f419a760b02
Dominique D <derrierdo@gmail.com>
2020-03-24 18:02:09 -0400

    BoidCmd

---
+++ Docker/test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+
+
+function title {
+    echo "# HELP $1 $2"
+    echo "# TYPE $1 $3"
+}
+
+
+function printMetric {
+    title "$1" "$2" "$3"
+    echo "$1$5 $4"
+}
+
+function printMetricA {
+    echo "$1$5 $4"
+}
+
+
+trap 'echo shutdown.; exit' INT
+
+function run() {
+while read -r line
+do
+line=$(echo "$line" | tr -d '\r\n')
+if [ -z "$line" ]
+	then
+	break
+	fi
+done
+
+echo -en "HTTP/1.0 200 OK\r\n"
+echo -en "Content-type: text/plain\r\n"
+echo -en "\r\n"
+
+succ=$(boinccmd --get_state |grep  succe | cut -d":" -f 2)
+fail=$(boinccmd --get_state |grep failed | cut -d":" -f 2)
+frac=$(boinccmd --get_state |grep fraction |cut -d':' -f 2)
+
+#printMetric "random" "Cool desc" "gauge" $RANDOM '{name="'$HOSTNAME'"}'; 
+printMetric "jobs" "Jobs done" "gauge" $succ '{name="'$HOSTNAME'",type="succ"}'
+printMetricA "jobs" "Jobs done" "gauge" $fail '{name="'$HOSTNAME'",type="fail"}'
+
+title "fraction" "fraction done" "gauge" 
+t=0
+for a in $frac
+do
+t=$(($t+1))
+printMetricA "fraction" "fraction done" "gauge" $a '{name="'$HOSTNAME'",task="'$t'"}'
+done
+
+}
+
+
+if [ ! "z$1" == "zdata" ]
+then
+	while :; do
+		nc -l -p 1234 -c './test.sh data'
+	done
+else
+run
+fi
+#
